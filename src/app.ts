baf5808de46140b0bd6de779c33536d1208416e6
rafaellamonier <rafaellamonier17@hotmail.com>
2026-05-22 19:15:09 -0300

    adjust route of health in app.ts

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => {
+app.get("/health", (req, res) => {
 	res.status(200).json({ status: "ok" });
 });
 
