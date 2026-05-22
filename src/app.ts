f4e6d1fd6d1b4685959d130acfd68ed8872e037a
rafaellamonier <rafaellamonier17@hotmail.com>
2026-05-22 18:26:17 -0300

    teste CORS

--- src/app.ts
+++ src/app.ts
@@ -6,27 +6,11 @@ import { supplierRoutes } from "./routes/supplier.routes";
 import cors from "cors";
 
 const app = express();
-const allowedOrigins = ["http://localhost:3333",
-    "https://cerimonialio.vercel.app/dashboard.app"];
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+app.use(cors());
 app.use(express.json());
 app.use("/users", userRoutes);
 app.use("/weddings", weddingRoutes);
 app.use("/suppliers", supplierRoutes);
 app.use(errorMiddleware);
-
-app.get("/health", (req, res) => {
-  return res.json({
-    status: "ok"
-  })
-})
 
 export { app };
